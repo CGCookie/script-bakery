03c8221134c0a3338336b32810868957fb7f153f
Jonathan Williamson <jonathan@cgcookie.com>
2013-01-30 20:16:45 -0800

    Add SubD Cube 0.2
    -Updating "Add SubD Cube" to 0.2
    
    - Improves modifier detection to no longer rely on the exact name.
    
    - Auto enable Optimal Display
    
    Signed-off-by: Jonathan Williamson <jonathan@cgcookie.com>

--- development/add-mesh_subd-cube.py
+++ development/add-mesh_subd-cube.py
@@ -3,7 +3,7 @@ bl_info = {
     "location": "View3D > Add > Mesh > Add SubD Cube",
     "description": "Adds ability to add a cube with a subdivision surface modifier already added",
     "author": "Jonathan Williamson",
-    "version": (0,1),
+    "version": (0,2),
     "blender": (2, 6, 5),
     "category": "Add Mesh",
     }
@@ -19,12 +19,21 @@ class addSubDCube(bpy.types.Operator):
     def execute(self, context):
         obj = context.active_object
         
-        addCube = bpy.ops.mesh.primitive_cube_add(view_align=False)
-        addSubD = bpy.ops.object.modifier_add(type='SUBSURF')
-        bpy.context.object.modifiers["Subsurf"].levels = 2
+        bpy.ops.mesh.primitive_cube_add(view_align=False)
+        bpy.ops.object.modifier_add(type='SUBSURF')
+                
+        obj = context.active_object
+        
+        activeMod = obj.modifiers
+        
+        for mod in obj.modifiers:
+            if mod.type == 'SUBSURF':
+                activeMod[mod.name].show_only_control_edges = True
+                activeMod[mod.name].levels = 2
         
       
         return {'FINISHED'}
+ 
 
 def menu_func(self, context):
     self.layout.operator(addSubDCube.bl_idname, icon='MOD_SUBSURF')
