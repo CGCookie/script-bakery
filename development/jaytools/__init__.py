c6a3c2d3d35197514a3438d17cf17aea0f920204
Jonathan Williamson <jonathan@cgcookie.com>
2013-03-08 09:55:17 -0800

    Added Tracker and Wiki URL
    - Added the URLs for the JayTools tracker and Wiki pages.
    
    Signed-off-by: Jonathan Williamson <jonathan@cgcookie.com>

--- development/jaytools/__init__.py
+++ development/jaytools/__init__.py
@@ -6,8 +6,8 @@ bl_info = {
     "blender": (2, 6, 6),
     "location": "View3D - 'Q' key gives a menu in Object, Edit, and Sculpt modes.",
     "warning": '',  # used for warning icon and text in addons panel
-    "wiki_url": "",
-    "tracker_url": "",
+    "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/Scripts/3D_interaction/jaytools",
+    "tracker_url": "http://projects.blender.org/tracker/index.php?func=detail&aid=34482&group_id=153&atid=467",
     "category": "3D View"}
 
 import sys, os
