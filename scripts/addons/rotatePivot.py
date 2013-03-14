ad6162efeb473563490d2990b3d261159176a2a6
Alex Telford <alex@metalix.co.nz>
2013-03-15 12:21:39 +1300

    Added rotatePivot and addonTemplate
    The rotate pivot script will rotate the pivot of all selected objects to
    be the same as the last selected, the addon template is a template file
    for quickly creating addons

---
+++ scripts/addons/rotatePivot.py
@@ -0,0 +1,105 @@
+#script template by Alex Telford | www.metalix.co.nz
+#script by Your Alex Telford | www.metalix.co.nz
+#Description:
+#Rotates the pivot of an object without effecting the object itself, caution: slow on heavy objects.
+bl_info = {
+    "name": "Script Name",
+    "author": "Alex Telford",
+    "version": (0, 1),
+    "blender": (2, 6, 6),
+    "location": "View3D > Toolshelf",
+    "description": "Rotate the pivot of an object",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "View3D"}
+    
+#import modules
+import bpy
+from bpy.props import *
+
+class rotatePivotInit(bpy.types.Panel):
+    bl_label = "Rotate Pivot"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    #mode that the panel will show in, OBJECT, SCULPT or EDIT
+    @classmethod
+    def poll(self, context):
+       return(bpy.context.mode == 'OBJECT')
+
+    def draw(self, context):
+        obj = context.object
+        scn = context.scene
+        
+        layout = self.layout
+        col = layout.column()
+        
+        #try to layout, otherwise say something else
+        try:
+            selected = bpy.context.scene.objects.active.name
+            failCheck = bpy.context.selected_objects[1]
+            #layout items
+            col.label("Copy Rotation from active(last selected) to all other selected")
+            col.label("Copy Rotation From:")
+            col.label('>>'+selected)
+            col.label("Copy Rotation To:")
+            for ob in bpy.context.selected_objects:
+                if ob != bpy.context.scene.objects.active:
+                    col.label('>>'+ob.name)
+            #execute button
+            layout.operator("copyrotation.toselected")
+        except:
+            #couldn't fill object list
+            col.label("Select an object")
+
+#run
+class OBJECT_OT_rotatePivot(bpy.types.Operator):
+    bl_idname = "copyrotation.toselected"
+    bl_label = "Rotate"
+    
+    def execute(self, context):
+        obj = context.object
+        scn = context.scene
+        
+        ###Run Script
+        #active
+        active = bpy.context.scene.objects.active
+        selection = bpy.context.selected_objects
+        bpy.ops.object.mode_set(mode='OBJECT')
+        bpy.ops.object.select_all(action='DESELECT')
+        for ob in selection:
+            if ob != active:
+                print(ob)
+                bpy.context.scene.objects.active = ob
+                ob.select = True
+                rotatePivot(active.rotation_euler)
+                bpy.ops.object.select_all(action='DESELECT')
+        #warnings use this context if you need them, here we just print the stuff
+        self.report({'INFO'}, "Done!")
+        
+        return{'FINISHED'}
+    
+###functions
+def rotatePivot(rotation):
+    #rotate selected object based on a vector
+    bpy.ops.object.mode_set(mode='OBJECT')
+    bpy.ops.transform.rotate(value=rotation.x, axis=(1,0,0))
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.transform.rotate(value=-rotation.x, axis=(1,0,0))
+    bpy.ops.object.mode_set(mode='OBJECT')
+    bpy.ops.transform.rotate(value=rotation.y, axis=(0,1,0))
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.transform.rotate(value=-rotation.y, axis=(0,1,0))
+    bpy.ops.object.mode_set(mode='OBJECT')
+    bpy.ops.transform.rotate(value=rotation.z, axis=(0,0,1))
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.transform.rotate(value=-rotation.z, axis=(0,0,1))
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+def register():
+    ###initialize classes
+    bpy.utils.register_class(rotatePivotInit)
+    bpy.utils.register_class(OBJECT_OT_rotatePivot)
+def unregister():
+    bpy.utils.unregister_class(rotatePivotInit)
+    bpy.utils.register_class(OBJECT_OT_rotatePivot)
