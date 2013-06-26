ba30a5a34159d498cdd5f24b1c96e0386fff5443
Jonathan Williamson <jonathan@cgcookie.com>
2013-06-25 21:46:26 -0700

    Cleanup Targeting consistency across modifiers.

--- scripts/addons/quicktools/quick_object_mode.py
+++ scripts/addons/quicktools/quick_object_mode.py
@@ -72,58 +72,7 @@ class QuickObjectOptions(bpy.types.Menu):
             layout.operator("object.shade_flat", icon='MESH_UVSPHERE')
         elif mode == 'EDIT':
             layout.operator("mesh.faces_shade_smooth", icon='SOLID')
-            layout.operator("mesh.faces_shade_flat", icon='MESH_UVSPHERE')
-        
-
-
-##################################   
-''' 
-I plan to retire the toolbar since it goes away from the "Quick" mentality that is the goal of these tools. 
-Just commenting it out for now in case I go back on my decision. 
-'''
-################################## 
-
-# # Create the Tool Bar section 
-# class QuickObjectToolbar(bpy.types.Panel):
-#     bl_label = "Quick Tools"
-#     bl_idname = "object.quick_object_toolbar"
-#     bl_space_type = 'VIEW_3D'
-#     bl_region_type = 'TOOLS'
-#     bl_context = 'objectmode'
-    
-#     def draw(self, context):
-#         layout = self.layout
-        
-#         col = layout.column(align=True)
-        
-#         col.label(text="Modifiers")
-        
-#         row = col.row()
-#         row.operator("object.add_subsurf", "Subsurf", icon="MOD_SUBSURF")
-#         row.operator("object.add_mirror", "Mirror", icon="MOD_MIRROR")
-        
-#         row = col.row()
-#         row.operator("object.apply_subsurf", "Apply Subsurf")
-#         row.operator("object.empty_add_unactive", "Add Target")
-        
-#         row = col.row()
-#         row.operator("object.apply_modifiers", "Apply Modifiers")
-        
-#         col.operator_menu_enum("object.modifier_add", "type")
-        
-#         col = layout.column(align=True)
-#         col.label(text="Origin")
-        
-#         row = col.row()
-#         row.operator_menu_enum("object.origin_set", "type", "Set Origin")
-        
-#         col = layout.column(align=True)
-#         col.label(text="Shading")
-
-#         row = col.row()
-#         row.operator("object.shade_smooth", "Smooth", icon='SOLID')
-#         row.operator("object.shade_flat", "Flat", icon='MESH_UVSPHERE')        
-        
+            layout.operator("mesh.faces_shade_flat", icon='MESH_UVSPHERE') 
         
   
 
